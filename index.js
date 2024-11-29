b3ff6e5067b651a5bc89aae5d364fef53de22101
Amin Khan <154885880+Aminkhan-mid@users.noreply.github.com>
2024-11-29 10:56:35 +0530

    added a new btn

--- index.js
+++ index.js
@@ -1,20 +1,36 @@
-const video = document.getElementById('myVideo')
-const playBtn = document.getElementById('playBtn')
-const pauseBtn = document.getElementById('pauseBtn')
+const videoOne = document.getElementById('myVideo')
+const videoTwo = document.getElementById('breathVideo')
+
 
 const toggleBtn = document.getElementById('toggleBtn')
+const toggleBtnEnergy = document.getElementById('toggleBtnEnergy')
 
+const takeBreathBtn = document.getElementById('quickBreath')
+const breathContainer =   document.getElementById('breathContainer')
 
 
 
 toggleBtn.addEventListener('click', () => {
-    if(video.paused){
-        video.play()
+    if(videoOne.paused){
+        videoOne.play()
         toggleBtn.textContent = 'Pause'
         toggleBtn.style.backgroundColor = '#e76f51'
     } else {
-        video.pause()
+        videoOne.pause()
         toggleBtn.textContent = 'Play'
         toggleBtn.style.backgroundColor = '#c1121f'
     }
 }) 
+
+toggleBtnEnergy.addEventListener('click', function(){
+    if(videoTwo.paused){
+        videoTwo.play()
+    } else{
+        videoTwo.pause()
+    }
+})
+
+
+takeBreathBtn.addEventListener('click', () => {
+    breathContainer.classList.toggle('hidden')
+})
